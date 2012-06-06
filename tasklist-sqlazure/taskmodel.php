e29773ab06900c466a1cbe2affae6a8c4e59bddc
Brian Swan <brian_swan@hotmail.com>
2012-06-06 16:52:18 -0700

    initial commit

---
+++ tasklist-sqlazure/taskmodel.php
@@ -0,0 +1,71 @@
+<?php
+/** * Copyright 2012 Microsoft Corporation 
+	*  
+	* Licensed under the Apache License, Version 2.0 (the "License"); 
+	* you may not use this file except in compliance with the License. 
+	* You may obtain a copy of the License at 
+	* http://www.apache.org/licenses/LICENSE-2.0 
+	*  
+	* Unless required by applicable law or agreed to in writing, software 
+	* distributed under the License is distributed on an "AS IS" BASIS, 
+	* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+	* See the License for the specific language governing permissions and 
+	* limitations under the License. 
+	*/
+	
+function connect()
+{
+	// DB connection info
+	$host = "<server name>\sqlexpress"; 
+	$user = "user name";
+	$pwd = "password";
+	$db = "tasklist";
+	try{
+		$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch(Exception $e){
+		die(print_r($e));
+	}
+	return $conn;
+}
+
+function markItemComplete($item_id)
+{
+	$conn = connect();
+	$sql = "UPDATE items SET is_complete = 1 WHERE id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $item_id);
+	$stmt->execute();
+}
+
+function getAllItems()
+{
+	$conn = connect();
+	$sql = "SELECT * FROM items";
+	$stmt = $conn->query($sql);
+	return $stmt->fetchAll(PDO::FETCH_NUM);
+}
+
+function addItem($name, $category, $date, $is_complete)
+{
+	$conn = connect();
+	$sql = "INSERT INTO items (name, category, date, is_complete) VALUES (?, ?, ?, ?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $name);
+	$stmt->bindValue(2, $category);
+	$stmt->bindValue(3, $date);
+	$stmt->bindValue(4, $is_complete);
+	$stmt->execute();
+}
+
+function deleteItem($item_id)
+{
+	$conn = connect();
+	$sql = "DELETE FROM items WHERE id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bindValue(1, $item_id);
+	$stmt->execute();
+}
+
+?>
